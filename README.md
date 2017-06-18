e5a3caacdc4fe34a798d89a592bb5c0d29135f52
Alan Fidelino <afidedev26@gmail.com>
2017-06-18 21:17:51 +1000

    Add Routing and README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Catch of the Day Application from WesBos React for Beginners Course
+
+This application used the following technologies:
+1. Firebase for Database storage and Authentication
+2. Stylus for styling
+3. React Router 4 for Routing
+4. React Animation
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { render } from 'react-dom';
+import {getFunName} from '../helpers'
+
 
 
 class StorePicker extends React.Component {
+  constructor(){
+    super();
+    this.selectStore = this.selectStore.bind(this);
+  }
+  selectStore(event){
+    event.preventDefault();
+
+    console.log(this.storeInput.value.toUpperCase());
+  }
   render(){
     return(
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.selectStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" ref={(input)=> this.storeInput = input} required placeholder="Store Name" defaultValue={ getFunName() } />
         <button type="submit">Visit Store > </button>
       </form>
     )
