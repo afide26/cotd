9aff2e782221641fc64ae13681a0501edb39f483
Alan Fidelino <afidedev26@gmail.com>
2017-06-18 20:36:11 +1000

    Create Components and import Style

---
+++ src/components/Inventory.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Inventory extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Hello from Inventory</h1>
+      </div>
+    )
+  }
+}
+
+export default Inventory;
---
+++ src/components/Order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Order extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Hello from Order</h1>
+      </div>
+    )
+  }
+}
+
+export default Order;
